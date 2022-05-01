9d695a1d6ed59c8b88a48c849be0dd1641f51f3c
LaraKra <konlor@rambler.ru>
2022-05-02 00:40:30 +0300

    add test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,23 +1,22 @@
 package ru.netology;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
+
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
         WebDriverManager.chromedriver().setup();
+
     }
 
     @BeforeEach
@@ -34,5 +33,17 @@ public class CallbackTest {
     public void tearDown() {
         driver.quit();
         driver = null;
+
+    }
+
+    @Test
+    public void shouldSendCorrectForm() {
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Анна Игнатьева");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
     }
 }
