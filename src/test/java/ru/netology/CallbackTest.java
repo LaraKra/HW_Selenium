68fda9bcab73067f2a2e231194c0502bbf848ce8
LaraKra <konlor@rambler.ru>
2022-05-02 00:45:34 +0300

    add test DoubleName

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -46,4 +46,14 @@ public class CallbackTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, actualText);
     }
+    @Test
+    public void shouldSendCorrectFormWithDoubleName() {
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Анна-Мария Петрова");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
+    }
 }
