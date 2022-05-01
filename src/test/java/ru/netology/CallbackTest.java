5c9ac5d0f91888d297092ef77bb01e2248a6f61d
LaraKra <konlor@rambler.ru>
2022-05-01 22:03:07 +0300

    add test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -6,37 +6,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-class ChromeTest {
 
-    WebDriver driver;
+public class CallbackTest {
+    private WebDriver driver;
 
     @BeforeAll
-    static void setupClass() {
+    public static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    void setupTest() {
-        driver = new ChromeDriver();
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://0.0.0.0:7777/");
     }
 
     @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void tearDown() {
+        driver.quit();
+        driver = null;
     }
-
-    @Test
-    void test() {
-        // Your test logic here
-    }
-
-}
-
-public class CallbackTest {
 }
